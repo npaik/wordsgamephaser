50a22ebda2d029d7586e317599c1b7dc457f6914
Tony Paik <tonypaik@Tonys-MacBook-Pro.local>
2023-04-27 20:35:52 -0700

    first commit

---
+++ game.js
@@ -0,0 +1,239 @@
+let gameScene = new Phaser.Scene("Game");
+
+let config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  scene: gameScene,
+};
+
+let game = new Phaser.Game(config);
+
+let timer;
+let planets;
+let scoreText;
+let score = 0;
+let planetsRemoved = 0;
+let gameTimeLimit = 30;
+let scoreFactor = 1;
+let typedWord = "";
+let timerEventAdded = false;
+let gameStarted = false;
+let gameOver = false;
+let typedWordText;
+
+gameScene.preload = function () {
+  this.load.image("background", "assets/background.png");
+  this.load.image("sun", "assets/sun.png");
+  this.load.image("mercury", "assets/mercury.png");
+  this.load.image("venus", "assets/venus.png");
+  this.load.image("earth", "assets/earth.png");
+  this.load.image("mars", "assets/mars.png");
+  this.load.image("jupiter", "assets/jupiter.png");
+  this.load.image("saturn", "assets/saturn.png");
+  this.load.image("uranus", "assets/uranus.png");
+  this.load.image("neptune", "assets/neptune.png");
+  this.load.image("meteorite", "assets/meteorite.png");
+  this.load.image("satellite", "assets/satellite.png");
+  this.load.image("startButton", "assets/startButton.png");
+};
+
+gameScene.create = function () {
+  this.add
+    .sprite(0, 0, "background")
+    .setPosition(
+      this.sys.game.config.width / 2,
+      this.sys.game.config.height / 2
+    );
+  scoreText = this.add.text(0, 0, "Score: 0");
+  timer = this.add.text(700, 0, `Timer: ${gameTimeLimit}`);
+  timer.depth = 1;
+  typedWordText = this.add.text(400, 480, "");
+  typedWordText.setOrigin(0.5); // This will center the text horizontally based on its position
+
+  this.add
+    .sprite(0, 0, "satellite")
+    .setScale(5)
+    .setAngle(320)
+    .setPosition(400, 550);
+
+  const meteorite = this.add
+    .sprite(0, 0, "meteorite")
+    .setScale(2)
+    .setAngle(220)
+    .setPosition(400, 430);
+  meteorite.visible = false;
+
+  planets = this.add.group();
+
+  const startButton = this.add
+    .sprite(
+      this.sys.game.config.width / 2,
+      this.sys.game.config.height / 2,
+      "startButton"
+    )
+    .setInteractive();
+
+  startButton.once("pointerdown", () => {
+    gameStarted = true;
+    startButton.setVisible(false);
+
+    if (!timerEventAdded) {
+      this.time.addEvent({
+        delay: 1000,
+        callback: updateTimer,
+        callbackScope: this,
+        loop: true,
+      });
+      timerEventAdded = true;
+    }
+
+    spawnPlanets.call(this);
+  });
+
+  this.input.keyboard.on("keydown", (event) => {
+    if (!gameOver && gameStarted) {
+      if (event.key === "Enter") {
+        checkWord.call(this);
+      } else if (event.key === "Backspace") {
+        typedWord = typedWord.slice(0, -1);
+      } else if (event.key === " ") {
+        typedWord += " ";
+      } else if (event.key.length === 1 && event.key.match(/[a-zA-Z]/)) {
+        typedWord += event.key;
+      }
+      typedWordText.setText(typedWord);
+    }
+  });
+
+  function checkWord() {
+    const targetTextContainer = planets.children.entries.find(
+      (textContainer) => {
+        const text = textContainer.list && textContainer.list[1];
+        return (
+          text &&
+          text.text &&
+          text.text.toLowerCase() === typedWord.toLowerCase()
+        );
+      }
+    );
+
+    if (targetTextContainer) {
+      const targetPlanet = planets.children.entries.find((planet) => {
+        return (
+          planet.texture &&
+          planet.texture.key &&
+          planet.texture.key.toLowerCase() === typedWord.toLowerCase()
+        );
+      });
+
+      if (targetPlanet) {
+        targetPlanet.destroy();
+      }
+
+      targetTextContainer.destroy();
+      moveMeteorite.call(this, targetTextContainer.x, targetTextContainer.y);
+
+      score += 1;
+      scoreText.setText(`Score: ${score}`);
+
+      planetsRemoved += 1;
+      if (planetsRemoved % 3 === 0) {
+        scoreFactor += 0.3;
+      }
+    }
+
+    typedWord = "";
+  }
+
+  function moveMeteorite(targetX, targetY) {
+    meteorite.visible = true;
+    meteorite.setPosition(400, 430);
+
+    this.tweens.add({
+      targets: meteorite,
+      x: targetX,
+      y: targetY,
+      duration: 500,
+      onComplete: () => {
+        meteorite.visible = false;
+      },
+    });
+  }
+};
+
+gameScene.update = function () {
+  if (!gameOver && gameStarted) {
+    planets.children.iterate(function (planet) {
+      planet.y += 0.2 * scoreFactor;
+    });
+  }
+};
+
+function updateTimer() {
+  if (gameTimeLimit > 0) {
+    gameTimeLimit -= 1;
+    timer.setText(`Timer: ${gameTimeLimit}`);
+  } else {
+    gameOver = true;
+  }
+}
+
+// function randomizeCase(str) {
+//   return str
+//     .split("")
+//     .map((char) => {
+//       return Math.random() < 0.5 ? char.toUpperCase() : char.toLowerCase();
+//     })
+//     .join("");
+// }
+
+function spawnPlanets() {
+  if (!gameOver) {
+    const planetKeys = [
+      "sun",
+      "mercury",
+      "venus",
+      "earth",
+      "mars",
+      "jupiter",
+      "saturn",
+      "uranus",
+      "neptune",
+    ];
+    const scaleFactors = [1.5, 2, 2, 1.5, 1, 0.8, 1, 1, 1];
+
+    const randomIndex = Math.floor(Math.random() * planetKeys.length);
+    const randomX = Math.random() * (this.sys.game.config.width - 100) + 50;
+    const randomDelay = Math.random() * 1000 + 500;
+
+    const planet = planets.create(randomX, -50, planetKeys[randomIndex]);
+    planet.setScale(scaleFactors[randomIndex]);
+
+    const textStyle = { font: "16px Arial", fill: "#ffffff" };
+    const text = this.add.text(0, 0, planetKeys[randomIndex], textStyle);
+    text.setOrigin(0.5, 0.5);
+
+    const textBackground = this.add.graphics();
+    textBackground.fillStyle(0x000000, 0.8);
+    textBackground.fillRect(
+      -text.width / 2 - 2,
+      -text.height / 2 - 2,
+      text.width + 4,
+      text.height + 4
+    );
+
+    const textContainer = this.add.container(randomX, -50, [
+      textBackground,
+      text,
+    ]);
+    textContainer.depth = 2;
+    planets.add(textContainer);
+
+    this.time.addEvent({
+      delay: randomDelay,
+      callback: spawnPlanets,
+      callbackScope: this,
+    });
+  }
+}
